648fa6d4b605a13668b2378c044e4aaaf5f1de43
barbaria888 <hardik.arora_cs22@gla.ac.in>
2026-02-10 12:01:50 +0000

    added

--- infrastructure/prerequesites.sh
+++ infrastructure/prerequesites.sh
@@ -1,8 +1,8 @@
-k apply -f frontend-deployment.yaml
-k apply -f frontend-service.yaml
-k apply -f backend-deployment.yaml
-k apply -f backend-service.yaml
-k apply -f mongo-pvc.yaml
-k apply -f mongo-deployment.yaml
-k apply -f mongo-service.yaml
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f frontend-service.yaml
+kubectl apply -f backend-deployment.yaml
+kubectl apply -f backend-service.yaml
+kubectl apply -f mongo-pvc.yaml
+kubectl apply -f mongo-deployment.yaml
+kubectl apply -f mongo-service.yaml
 
