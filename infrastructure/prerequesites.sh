f2ac8b6d940aaa441035394cf94b37a0e33dd3a3
HARDIK ARORA <arorahardik0811@gmail.com>
2026-02-22 15:24:37 +0530

    Add Helm and Prometheus setup to prerequisites script
    Added Helm installation and Prometheus setup commands.

--- infrastructure/prerequesites.sh
+++ infrastructure/prerequesites.sh
@@ -5,4 +5,11 @@ kubectl apply -f backend-service.yaml
 kubectl apply -f mongo-pvc.yaml
 kubectl apply -f mongo-deployment.yaml
 kubectl apply -f mongo-service.yaml
-
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install monitoring prometheus-community/kube-prometheus-stack \
+  --namespace monitoring \
+  --create-namespace
+kubectl get svc -n monitoring
+kubectl port-forward svc/monitoring-grafana 3000:80 -n monitoring
