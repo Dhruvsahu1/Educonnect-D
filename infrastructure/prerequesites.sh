78222720ca11c1e4f3c30e88470cce9f8de6ccbf
HARDIK ARORA <arorahardik0811@gmail.com>
2026-03-03 03:01:08 +0530

    Update prerequesites.sh

--- infrastructure/prerequesites.sh
+++ infrastructure/prerequesites.sh
@@ -2,14 +2,10 @@ kubectl apply -f frontend-deployment.yaml
 kubectl apply -f frontend-service.yaml
 kubectl apply -f backend-deployment.yaml
 kubectl apply -f backend-service.yaml
+kubectl apply -f ingress.yaml
 kubectl apply -f mongo-pvc.yaml
 kubectl apply -f mongo-deployment.yaml
 kubectl apply -f mongo-service.yaml
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install monitoring prometheus-community/kube-prometheus-stack \
-  --namespace monitoring \
-  --create-namespace
-kubectl get svc -n monitoring
-kubectl port-forward svc/monitoring-grafana 3000:80 -n monitoring
