6d01d99a5c03495fde802edd9943c9775a3b3b9d
HARDIK ARORA <arorahardik0811@gmail.com>
2026-03-03 03:06:34 +0530

    Update API_URL handling in docker-entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -e
 
-# Get the API URL from environment variable, default to Kubernetes service URL
-API_URL=${API_URL:-http://backend:5000/api}
-
-echo "Replacing __API_URL__ with: $API_URL"
-
-# Replace the placeholder in the built JS files
+API_URL=${VITE_API_URL:-/api}
+echo "Replacing __API_URL__ with $API_URL"
 find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|__API_URL__|${API_URL}|g" {} \;
 
-echo "Starting nginx..."
 exec nginx -g "daemon off;"
