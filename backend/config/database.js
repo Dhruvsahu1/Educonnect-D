b8079d5bb3d23a5c85cab5e8ee6756eadae74462
HARDIK ARORA <arorahardik0811@gmail.com>
2026-04-14 22:57:08 +0530

    Skip DB connection if SKIP_DB is true
    Add conditional to skip database connection in CI mode.

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    // skipping db connection in DAST
+    if (process.env.SKIP_DB === "true") {
+      console.warn("⚠️ Skipping DB connection (CI mode)");
+      return;
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
