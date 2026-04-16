22ead6f8612ba713000940f5ae88bb002c719d75
HARDIK ARORA <arorahardik0811@gmail.com>
2026-04-17 02:51:34 +0530

    Update main.tf

--- Infrastructure-as_Code/scan/main.tf
+++ Infrastructure-as_Code/scan/main.tf
@@ -11,7 +11,7 @@ module "network" {
 
 
 module "vm" {
-  source              = "./modules/vm"
+  source              = "./modules/compute"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   vm_name        = var.vm_name
