94e3cca6cf7cda0709ef046c426d8f08cbb47606
HARDIK ARORA <arorahardik0811@gmail.com>
2026-04-18 03:10:44 +0530

    Update main.tf

--- Infrastructure-as_Code/scan/main.tf
+++ Infrastructure-as_Code/scan/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+
 module "network" {
   source              = "./modules/network"
   resource_group_name = azurerm_resource_group.rg.name
